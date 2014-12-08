d8ce204479fb5db96a52a459d1e839ec655083fe
Gabe Fierro <gtfierro225@gmail.com>
2014-12-08 14:20:56 -0800

    start adding interface of operations

--- gobad/operations.go
+++ gobad/operations.go
@@ -1,10 +1,14 @@
 package gobad
 
+// currently using bson.M as a placeholder for a "universal" return type
 import (
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Operation interface {
+	// get a single document by using a unique identifier
 	GetDocumentUnique(uuid string) bson.M
+
+	// get a set of documents using a where clause
 	GetDocumentSetWhere(where bson.M) []bson.M
 }
