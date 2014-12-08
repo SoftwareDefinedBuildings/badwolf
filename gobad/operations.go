84e35fb7fc7d8e01d7a36698c3abb0f8a10b3a8b
Gabe Fierro <gtfierro225@gmail.com>
2014-12-08 14:27:58 -0800

    add more functions

--- gobad/operations.go
+++ gobad/operations.go
@@ -11,4 +11,13 @@ type Operation interface {
 
 	// get a set of documents using a where clause
 	GetDocumentSetWhere(where bson.M) []bson.M
+
+    // get list of unique values for a given key
+    GetUniqueValues(key string) []interface{}
+
+    // get a set of documents with a key/value matching a glob (anchored regex)
+    GetDocumentSetValueGlob(key, value_glob string) []bson.M
+
+    // get a set of keys that match a glob
+    GetKeyGlob(key_glob string) []string
 }
