f9af385f88b8c9948e184358cbccd861eb647fd7
Gabe Fierro <gtfierro225@gmail.com>
2014-12-08 14:45:07 -0800

    change bson.M to kvlist

--- gobad/operations.go
+++ gobad/operations.go
@@ -1,24 +1,22 @@
 package gobad
 
-// currently using bson.M as a placeholder for a "universal" return type
-import (
-	"gopkg.in/mgo.v2/bson"
-)
+// a document is a list of key/value pairs
+type KVList [][2]string
 
 type Operation interface {
 	// Get Operations
 
 	// get a single document by using a unique identifier
-	GetDocumentUnique(uuid string) (bson.M, error)
+	GetDocumentUnique(uuid string) (KVList, error)
 
 	// get a set of documents using a where clause
-	GetDocumentSetWhere(where bson.M) ([]bson.M, error)
+	GetDocumentSetWhere(where KVList) ([]KVList, error)
 
 	// get list of unique values for a given key
 	GetUniqueValues(key string) ([]interface{}, error)
 
 	// get a set of documents with a key/value matching a glob (anchored regex)
-	GetDocumentSetValueGlob(key, value_glob string) ([]bson.M, error)
+	GetDocumentSetValueGlob(key, value_glob string) ([]KVList, error)
 
 	// get a set of keys that match a glob
 	GetKeyGlob(key_glob string) ([]string, error)
@@ -26,16 +24,16 @@ type Operation interface {
 	// Set Operations
 
 	// insert list of documents
-	InsertDocument(docs []bson.M) error
+	InsertDocument(docs []KVList) error
 
 	// set k/v pairs in unique document
-	SetKVDocumentUnique(kv bson.M, uuid string) error
+	SetKVDocumentUnique(kv KVList, uuid string) error
 
 	// set k/v pairs in set of documents using where clause
-	SetKVDocumentWhere(kv, where bson.M) error
+	SetKVDocumentWhere(kv, where KVList) error
 
 	// set k/v pairs for set of documents with k/v matching glob
-	SetKVDocumentValueGlob(kv bson.M, key, value_glob string) error
+	SetKVDocumentValueGlob(kv KVList, key, value_glob string) error
 
 	// Delete Operations
 
@@ -43,11 +41,11 @@ type Operation interface {
 	DeleteKeyDocumentUnique(keys []string, uuid string) error
 
 	// delete list of keys in set of documents using where clause
-	DeleteKeyDocumentWhere(keys []string, where bson.M) error
+	DeleteKeyDocumentWhere(keys []string, where KVList) error
 
 	// delete keys that match glob in unique document
 	DeleteKeyGlobDocumentUnique(key_glob, uuid string) error
 
 	// delete keys that match glob in set of documents using where clause
-	DeleteKeyGlobDocumentWhere(key_glob string, where bson.M) error
+	DeleteKeyGlobDocumentWhere(key_glob string, where KVList) error
 }
