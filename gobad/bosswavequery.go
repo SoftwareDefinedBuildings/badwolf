75d68a76a2c0ed12ebb0089060708fb8078f17e2
Michael Andersen <m.andersen@cs.berkeley.edu>
2014-12-08 15:06:01 -0800

    Adding bw query

---
+++ gobad/bosswavequery.go
@@ -0,0 +1,39 @@
+package gobad
+
+type BosswaveRecord struct {
+	Key      string
+	AllocSet int64
+	Owner    int64
+	Size     int64
+	Value    []byte
+}
+
+type VK []byte //32
+
+type AllocationSet struct {
+	Owner VK
+	Id    int64
+}
+
+type BosswaveQuery interface {
+
+	//Get a specific value
+	GetRecord(key string) BosswaveRecord
+
+	//Insert a record
+	InsertRecord(r BosswaveRecord)
+	
+	//Get a list of keys up to a slash
+	//so GetKeysUpToSlash(/foo/bar/) would return /foo/bar/baz
+	//but not /foo/bar/baz/box
+	GetKeysUpToSlash(keyprefix string) []string
+
+	//Get sum(size) for all records with the given allocation set
+	SumSize(AllocSet int64) int64
+
+	//Create an allocation set
+	CreateAllocSet(r AllocationSet)
+
+	//Get the allocation set ID
+	GetAllocSetID(vk VK) int64
+}
