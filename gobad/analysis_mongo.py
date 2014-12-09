b8c65be261ab65c468da178425deb734a7f36c54
Gabe Fierro <gtfierro225@gmail.com>
2014-12-09 15:58:51 -0800

    plot and save to pdf

--- gobad/analysis_mongo.py
+++ gobad/analysis_mongo.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 import json
 import sys
 
@@ -11,7 +12,12 @@ d = pd.DataFrame.from_records(data['metrics'])
 
 for id_name, groupdata in d.groupby('id'):
     print id_name
-    print (groupdata['value'] / groupdata['iteration'].max()).describe()
-    #ax = groupdata.plot(kind='line',x='iteration', y='value', legend=False)
-    #ax.set_ylabel('Latency (us)')
-    #ax.set_title(id_name)
+    groupdata['value'] /= groupdata['iteration'].max()
+    print groupdata['value'].describe()
+    plt.clf()
+    ax = groupdata.plot(kind='line',x='iteration', y='value', figsize=(24,16), legend=False, table=groupdata['value'].describe())
+    ax.set_ylabel('Latency (us)')
+    ax.set_xlabel('')
+    ax.set_title(id_name)
+    fig = ax.get_figure()
+    fig.savefig('{0}.pdf'.format(id_name))
