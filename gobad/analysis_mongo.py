3efc986400ba590eeceb4b0e92b0b592455e8ca2
Gabe Fierro <gtfierro225@gmail.com>
2014-12-09 17:41:10 -0800

    add violin plots

--- gobad/analysis_mongo.py
+++ gobad/analysis_mongo.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import json
 import sys
+import seaborn as sns
 
 data = json.load(open('benchmarkresult.json'))
 if not data['ok']:
@@ -9,6 +10,18 @@ if not data['ok']:
     sys.exit(1)
 
 d = pd.DataFrame.from_records(data['metrics'])
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+
+#TODO: we are going to want to compare plots across providers, so we might need another key
+ids = d['id'].unique()
+for idx, chunk in enumerate(chunker(ids, 4)):
+    tmp = d[d['id'].isin(chunk)]
+    ax = sns.violinplot(tmp['value'], tmp['id'], figsize=(24,16))
+    fig = ax.get_figure()
+    fig.savefig('violin{0}.pdf'.format(idx))
+
 
 for id_name, groupdata in d.groupby('id'):
     print id_name
